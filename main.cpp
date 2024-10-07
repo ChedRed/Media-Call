b42707b4f8f58942988dc9bbcc911ae21363f09f
Ryan C <rjchou@icloud.com>
2024-10-06 21:11:46 -0700

    Try on windows

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include "main.h"
+#include <X11/Xlib.h>
 
+
+int w, h;
+bool loop = true;
 
 /* Program main */
 int main() {
+    SDL_Spine app;
 
+
+    app.SDL_PInit("Media Call", { 720, 425 }, { 720, 425 }, SDL_INIT_VIDEO, SDL_WINDOW_ALWAYS_ON_TOP | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_UTILITY);
+    while (loop) {
+        while (SDL_PollEvent(&app.e) != 0) {
+            switch (app.e.type) {
+                case SDL_EVENT_QUIT:
+                    loop = false;
+                    break;
+
+                case SDL_EVENT_WINDOW_RESIZED:
+                    SDL_GetWindowSize(app.window, &w, &h);
+                    break;
+            }
+        }
+
+        app.SDL_PRend({ 32, 32, 32, 255 });
+    }
+
+    app.SDL_PQuit();
 
     return 0;
 }
--- main.h
+++ main.h
@@ -1,4 +1,6 @@
+#pragma  once
 #include <iostream>
+#include <string.h>
 #include <SDL3/SDL.h>
 
 
@@ -15,19 +17,40 @@ SDL_Window * window;
 SDL_Renderer * renderer;
 SDL_Event e;
 SDL_Spine();
-void SDL_PInit(SDL_InitFlags initflags, const char * name, vec2 size, SDL_WindowFlags winflags);
-void SDL_PRend();
+void SDL_PInit(const char * name, vec2 windimensions, vec2 winmindimensions, SDL_InitFlags initflags, SDL_WindowFlags winflags);
+void SDL_PRend(SDL_Color color);
 void SDL_PQuit();
+std::string osname = "undefined";
 };
 
 
 inline SDL_Spine::SDL_Spine() {
-
+    #ifdef _WIN32
+        osname = "windows";
+        // #include <windows.h>
+    #elif __APPLE__
+        osname = "macosx";
+        // SDL_SetPointerProperty(SDL_GetWindowProperties(window), "SDL.window.cocoa.window.ignoresMouseEvents", (void * )true);
+    #elif __linux__
+        osname = "linux";
+        // #include <X11/Xlib.h>
+        // #include <X11/Xatom.h>
+    #elif __unix__
+        osname = "Unix";
+    #endif
 }
 
-inline void SDL_Spine::SDL_PInit(SDL_InitFlags initflags, const char * name, vec2 size, SDL_WindowFlags winflags) {
+inline void SDL_Spine::SDL_PInit(const char * name, vec2 windimensions, vec2 winmindimensions, SDL_InitFlags initflags, SDL_WindowFlags winflags) {
     SDL_Init(initflags);
-    SDL_CreateWindowAndRenderer(name, size.x, size.y, winflags, &window, &renderer);
+    SDL_CreateWindowAndRenderer(name, windimensions.x, windimensions.y, winflags, &window, &renderer);
+    SDL_SetWindowMinimumSize(window, winmindimensions.x, winmindimensions.y);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+}
+
+inline void SDL_Spine::SDL_PRend(SDL_Color color) {
+    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     SDL_RenderClear(renderer);
 }
 
